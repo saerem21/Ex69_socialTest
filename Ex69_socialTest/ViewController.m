20f797cab9f81d8511d8586a7645cffa9868cc6e
saerem21 <nemessisdds>
2014-01-21 13:11:12 +0900

    페이스북 테스트입니다.

--- Ex69_socialTest/ViewController.m
+++ Ex69_socialTest/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import <Social/Social.h>
+
 @interface ViewController ()
 
 @end
@@ -15,14 +17,33 @@
 @implementation ViewController
 
 - (IBAction)showActivityVC:(id)sender{
-    UIImage *image = [UIImage imageNamed:@"ball1.png"];
-    NSArray *items = @[@"activity view test",image];
-    UIActivityViewController *vc = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:nil];
-    vc.completionHandler = ^(NSString *activityType,BOOL completed){
-        NSLog(@"%@ 의 동작을 마쳤습니다.",activityType);
-    };
+    NSString *service = SLServiceTypeFacebook;
+    if([SLComposeViewController isAvailableForServiceType:service]){
+        SLComposeViewController *composer = [SLComposeViewController composeViewControllerForServiceType:service];
+        UIImage *image = [UIImage imageNamed:@"ball1.png"];
+        [composer addImage:image];
+        [composer setInitialText:@"소셜 테스트"];
+        
+        composer.completionHandler = ^(SLComposeViewControllerResult result){
+            if(SLComposeViewControllerResultDone == result){
+                NSLog(@"complete");
+            }
+            else{
+                NSLog(@"exit");
+            }
+        };
+        [self presentViewController:composer animated:YES completion:nil];
+
+    }
+//    
+//    UIImage *image = [UIImage imageNamed:@"ball1.png"];
+//    NSArray *items = @[@"activity view test",image];
+//    UIActivityViewController *vc = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:nil];
+//    vc.completionHandler = ^(NSString *activityType,BOOL completed){
+//        NSLog(@"%@ 의 동작을 마쳤습니다.",activityType);
+//    };
     
-    [self presentViewController:vc animated:YES completion:nil];
+    //[self presentViewController:vc animated:YES completion:nil];
 }
 
 - (void)viewDidLoad
